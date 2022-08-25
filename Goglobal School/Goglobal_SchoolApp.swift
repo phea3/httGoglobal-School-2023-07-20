8945e02c8b82e5dec8f36b8da4654a0446ee4492
Mouygech Leang <leangmouygech68@gmail.com>
2022-08-25 08:16:22 +0700

    Initial Commit

---
+++ Goglobal School/Goglobal_SchoolApp.swift
@@ -0,0 +1,17 @@
+//
+//  Goglobal_SchoolApp.swift
+//  Goglobal School
+//
+//  Created by Leng Mouyngech on 25/8/22.
+//
+
+import SwiftUI
+
+@main
+struct Goglobal_SchoolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
